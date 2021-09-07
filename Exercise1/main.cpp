76f1c8087de563645e3ae9825b0f85b13722c547
Márton Leitold <lmarti1995@gmail.com>
2021-09-07 11:49:35 +0200

    Adding reading in and external libraries for xml

--- Exercise1/main.cpp
+++ Exercise1/main.cpp
@@ -1,20 +1,18 @@
-// Exercise1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include "pugixml.hpp"
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    pugi::xml_document doc;
+
+    pugi::xml_parse_result result = doc.load_file("Exercise1/small.xml");
+    if(!result)
+        return -1;
+
+    for(pugi::xml_node task : doc.child("Model").child("Application").children("Task"))
+    {
+        int timeout = task.attribute("Deadline").as_int();
+
+        std::cout << timeout << std::endl;
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
