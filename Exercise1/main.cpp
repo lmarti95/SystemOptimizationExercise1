79c7d4e02a3b91282acc189e23f7c63e3439070c
Márton Leitold <lmarti1995@gmail.com>
2021-09-13 15:00:09 +0200

    Adding data structures and read in capability

--- Exercise1/main.cpp
+++ Exercise1/main.cpp
@@ -1,5 +1,28 @@
 #include "pugixml.hpp"
+
 #include <iostream>
+#include <vector>
+
+struct Task {
+    int mDeadline;
+    int mId;
+    int mPeriod;
+    int mWcet;
+}typedef Task;
+
+struct Core {
+    int mId;
+    double mWcetFactor;
+}typedef Core;
+
+struct MCP {
+    int mId;
+    std::vector<Core> mCores;
+}typedef MCP;
+
+
+std::vector<Task> tasks;
+std::vector<MCP> platform;
 
 int main()
 {
@@ -7,12 +30,31 @@ int main()
 
     pugi::xml_parse_result result = doc.load_file("Exercise1/small.xml");
     if(!result)
-        return -1;
+        std::cout << "Didn't find the specified file" << std::endl;
 
-    for(pugi::xml_node task : doc.child("Model").child("Application").children("Task"))
+
+    for(pugi::xml_node readinTask : doc.child("Model").child("Application").children("Task"))
     {
-        int timeout = task.attribute("Deadline").as_int();
+        Task t;
+        t.mDeadline = readinTask.attribute("Deadline").as_int();
+        t.mId = readinTask.attribute("Id").as_int();
+        t.mPeriod = readinTask.attribute("Period").as_int();
+        t.mWcet = readinTask.attribute("WCET").as_int();
 
-        std::cout << timeout << std::endl;
+        tasks.push_back(t);
+    }
+
+    for(pugi::xml_node readinMCP : doc.child("Model").child("Platform").children("MCP"))
+    {
+        MCP mcp;
+        mcp.mId = readinMCP.attribute("Id").as_int();
+        for(pugi::xml_node readinCore : readinMCP.children())
+        {
+            Core c;
+            c.mId = readinCore.attribute("Id").as_int();
+            c.mWcetFactor = readinCore.attribute("WCETFactor").as_double();
+            mcp.mCores.push_back(c);
+        }
+        platform.push_back(mcp);
     }
 }
