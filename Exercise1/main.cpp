000b29d788755798fd104aab7748fcf8905bcadb
Márton Leitold <lmarti1995@gmail.com>
2021-09-15 18:34:50 +0200

    Adding initial solution generation

--- Exercise1/main.cpp
+++ Exercise1/main.cpp
@@ -1,6 +1,8 @@
 #include "pugixml.hpp"
 
 #include <iostream>
+#include <random>
+#include <utility>
 #include <vector>
 
 struct Task {
@@ -8,6 +10,7 @@ struct Task {
     int mId;
     int mPeriod;
     int mWcet;
+    double mPriority;
 }typedef Task;
 
 struct Core {
@@ -24,7 +27,7 @@ struct MCP {
 std::vector<Task> tasks;
 std::vector<MCP> platform;
 
-int main()
+void readIn()
 {
     pugi::xml_document doc;
 
@@ -40,6 +43,7 @@ int main()
         t.mId = readinTask.attribute("Id").as_int();
         t.mPeriod = readinTask.attribute("Period").as_int();
         t.mWcet = readinTask.attribute("WCET").as_int();
+        t.mPriority = 1.0 / (double)t.mDeadline;
 
         tasks.push_back(t);
     }
@@ -57,4 +61,53 @@ int main()
         }
         platform.push_back(mcp);
     }
+    std::cout << "Read in success" << std::endl;
+}
+
+bool checkIfAllCoreHasTasks(std::vector<std::pair<std::string, std::string>> solution)
+{
+    for(unsigned i = 0; i < platform.size(); ++i)
+    {
+        for(unsigned j = 0; j < platform.at(i).mCores.size(); ++j)
+        {
+            if(std::find_if(solution.begin(), solution.end(), [i, j](std::pair<std::string, std::string > pair){
+                return pair.second.compare(std::to_string(i) + "." + std::to_string(j)) == 0;
+                }) == solution.end())
+                return false;
+        }
+    }
+
+    return true;
+}
+
+//first task id, second mcpid.coreid
+std::vector<std::pair<std::string, std::string>> createInitialSolution()
+{
+    std::vector<std::pair<std::string, std::string>> solution;
+
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> mcpRandom(0, platform.size()-1);
+    while(!checkIfAllCoreHasTasks(solution))
+    {
+        solution.clear();
+        for(unsigned i = 0; i < tasks.size(); ++i)
+        {
+            int mcp = mcpRandom(rng);
+            std::uniform_int_distribution<std::mt19937::result_type> coreRandom(0, platform.at(mcp).mCores.size() - 1);
+            int core = coreRandom(rng);
+
+            solution.push_back(std::make_pair(std::to_string(i), std::to_string(mcp) + "." + std::to_string(core)));
+        }
+    }
+    
+    std::cout << "Initial solution created" << std::endl;
+    return solution;
+}
+
+int main()
+{
+    readIn();
+
+    createInitialSolution();
 }
